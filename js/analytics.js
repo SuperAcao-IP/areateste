65e78364dc42376e8705ba49ebd7c088b9c088dc
SuperAcao-IP <beatriz.ribas@fgv.br>
2026-08-04 15:05:14 -0300

    Fix API URL assignment in analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -18,7 +18,7 @@
   'use strict';
 
   /* ====================== AJUSTE AQUI ====================== */
-  var API = 'https://superacao-analytics.beatrizgribas.workers.dev';
+var API = 'https://superacao-analytics.beatrizgribas.workers.dev';
   /* ========================================================= */
 
   if (API.indexOf('SEU-SUBDOMINIO') !== -1) {
